2557bf65cfdfd3bcbf673e23e1f1ee76ad13d0ec
DevAhmedKhalil-laptop <dev.akhalil01@gmail.com>
2025-12-01 21:14:41 +0200

    5- handling evnets

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,21 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let cites = ["New York", "San Francisco", "Tokyo", "London", "Paris", "Rome"];
 
-  cites = [];
+  // Event Handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>My best countries: </h1>
 
-      {/* {cites.length === 0 ? <p>No cities found</p> : null} */}
       {cites.length > 0 && <p>Showing {cites.length} cities</p>}
-      {/* true && 1 = 1, true && 0 = 0, true && ahmed = ahmed, false && ahmed = false */}
+      {/* true && 1 = 1 || true && 0 = 0 || true && ahmed = ahmed || false && ahmed = false */}
 
       <ul className="list-group ">
-        {cites.map((city) => (
-          <li key={city} className="list-group-item">
+        {cites.map((city, index) => (
+          <li key={city} className="list-group-item" onClick={handleClick}>
             {city}
           </li>
         ))}
