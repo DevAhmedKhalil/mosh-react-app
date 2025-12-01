613672baf920917b7b971d2f1ce1dbcb9af85aeb
DevAhmedKhalil-laptop <dev.akhalil01@gmail.com>
2025-12-01 16:58:18 +0200

    4- conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,16 @@
 function ListGroup() {
-  const cites = [
-    "New York",
-    "San Francisco",
-    "Tokyo",
-    "London",
-    "Paris",
-    "Rome",
-  ];
+  let cites = ["New York", "San Francisco", "Tokyo", "London", "Paris", "Rome"];
+
+  cites = [];
 
   return (
     <>
       <h1>My best countries: </h1>
+
+      {/* {cites.length === 0 ? <p>No cities found</p> : null} */}
+      {cites.length > 0 && <p>Showing {cites.length} cities</p>}
+      {/* true && 1 = 1, true && 0 = 0, true && ahmed = ahmed, false && ahmed = false */}
+
       <ul className="list-group ">
         {cites.map((city) => (
           <li key={city} className="list-group-item">
