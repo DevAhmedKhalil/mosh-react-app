57269d1359f9b1617c69ac0dae97ddb777f18618
DevAhmedKhalil-laptop <dev.akhalil01@gmail.com>
2025-12-03 14:09:45 +0200

    10- building like component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import Button from "./components/Button/Button";
 import ListGroup from "./components/ListGroup";
-import { FaCity } from "react-icons/fa";
+import { FaCity, FaHeart } from "react-icons/fa";
 
 function App() {
+  const [liked, setLiked] = useState(false); // ← ده الـ state الحقيقي
+
   const cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
 
   return (
     <div>
+      <FaHeart
+        size={40}
+        color={liked ? "red" : "gray"} // ← يتغير تلقائي
+        onClick={() => {
+          setLiked(!liked);
+          console.log("clicked");
+        }} // ← يغير الـ state
+      />
+
+      <br />
+
       <FaCity color="blue" size={40} />
       <ListGroup
         items={cities}
