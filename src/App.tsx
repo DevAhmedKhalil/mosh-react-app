479f8914614682203c599176d0a1d8d4097ed304
DevAhmedKhalil-laptop <dev.akhalil01@gmail.com>
2025-12-03 13:38:17 +0200

    5-8 addin icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import ListGroup from "./components/ListGroup";
+import { FaCity } from "react-icons/fa";
 
 function App() {
   const cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
 
   return (
     <div>
+      <FaCity color="blue" size={40} />
+
       <ListGroup
         items={cities}
         heading="Cities"
