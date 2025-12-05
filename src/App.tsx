c0c75ddae9337c39fa0a52c7e48a7bd10a1950f8
DevAhmedKhalil-laptop <dev.akhalil01@gmail.com>
2025-12-05 21:05:59 +0200

    sec-5: exercise update state 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,39 @@
 import { useState } from "react";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.5,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
-
   const handleClick = () => {
-    setPizza({
-      ...pizza,
-      toppings: [...pizza.toppings, "Checken"],
+    // Update the quantity of the first item in the cart
+    setCart({
+      ...cart,
+      items: cart.items.map((item) => {
+        // if (item.id === 1) {
+        //   return { ...item, quantity: item.quantity + 1 };
+        // }
+        // return item;
+
+        return item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item;
+      }),
     });
   };
 
   return (
     <div>
-      <h1>Pizza</h1>
-      {pizza.name}
+      <h1>Cart: {cart.items.length} items</h1>
+      {cart.items.map((item) => (
+        <div key={item.id}>
+          {item.title} - Quantity: {item.quantity}
+        </div>
+      ))}
       <br />
-      {pizza.toppings.map((p) => p)}
       <br />
-      <br />
-      <button onClick={handleClick}>Adding Toppings To Pizza </button>
+      <button onClick={handleClick}>Add quantity</button>
     </div>
   );
 }
