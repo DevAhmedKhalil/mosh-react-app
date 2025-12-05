a98c65ef18c875c7e49159422fcfe154766bef70
DevAhmedKhalil-laptop <dev.akhalil01@gmail.com>
2025-12-05 17:15:43 +0200

    sec-5: updating nested objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,37 @@
 import { useState } from "react";
 
 function App() {
-  const [drink, setDrink] = useState({
-    title: "Caffee",
-    price: 25,
+  const [customer, setCustomer] = useState({
+    name: "ahmed",
+    address: {
+      city: "cairo",
+      zipcode: 12345,
+    },
   });
 
   const handleClick = () => {
-    //? Update drink price
     //@ When dealing with [objects & arrays] =>
     //@ you shoud remember to treat them as immutable or read only.
+    //? Update nested objests
 
-    const newDrink = {
-      // title: drink.title,
-      ...drink,
-      price: drink.price + 5,
-    };
+    // setCustomer({...customer});
+    //# This object with refrence the same address on the memoryso
 
-    setDrink(newDrink);
+    setCustomer({
+      ...customer,
+      address: { ...customer.address, zipcode: 111555 },
+    });
+    //# We want to make sure that the new obj is independent of the exsting object
   };
 
   return (
     <div>
-      {"The Drink is: " + drink.title}
-      {"The Price is: " + drink.price}
+      {"My name is: " +
+        customer.name +
+        " I live in: " +
+        customer.address.city +
+        " zip code is: " +
+        customer.address.zipcode}
       <br />
       <br />
       <button onClick={handleClick}>Update The Price</button>
