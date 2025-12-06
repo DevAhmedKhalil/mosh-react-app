91e51c403e829cb18ad4695d1036cb2aed05c190
DevAhmedKhalil-laptop <dev.akhalil01@gmail.com>
2025-12-06 21:17:41 +0200

    sec-6: building the form

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import { useState } from "react";
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText maxChars={10}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-        facilis quam corporis corrupti ullam, rerum iusto hic porro asperiores
-        nihil voluptatem odio, ipsum, eaque tempora adipisci incidunt dicta.
-        Soluta perferendis molestiae deleniti ipsam asperiores labore temporibus
-        neque molestias. Dicta numquam esse magnam amet unde rem expedita, nemo
-        commodi obcaecati provident, autem reiciendis architecto maxime in eum
-        nulla placeat doloribus consectetur inventore maiores facilis blanditiis
-        ad. Eveniet officiis necessitatibus cupiditate accusantium autem earum
-        harum, optio molestias? Facere quos deserunt ut dolorum, totam sed nam
-        fugiat quis dolor neque? Architecto iure aspernatur sit consequuntur
-        omnis rem, eos ipsum maiores repellendus vitae dicta.
-      </ExpandableText>
+      <Form />
     </div>
   );
 }
