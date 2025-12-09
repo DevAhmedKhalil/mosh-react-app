189084b940719f841e5ce49da091bf15ffbdf0dc
DevAhmedKhalil-laptop <dev.akhalil01@gmail.com>
2025-12-09 16:40:45 +0200

    sec-7: understanding the effict hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,23 @@
-import { useState } from "react";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-import cagegories from "./expense-tracker/categories";
+import { useEffect, useRef } from "react";
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
+  const ref = useRef<HTMLInputElement>(null);
 
-  const [expences, setExpences] = useState([
-    { id: 1, description: "Groceries", amount: 50, category: "Food" },
-    {
-      id: 2,
-      description: "Electricity Bill",
-      amount: 75,
-      category: "Utilities",
-    },
-    {
-      id: 3,
-      description: "Internet Subscription",
-      amount: 40,
-      category: "Entertainment",
-    },
-    { id: 4, description: "Gym Membership", amount: 30, category: "Health" },
-  ]);
+  // After Render
+  useEffect(() => {
+    // Side Effect: Focus the input element when the component mounts
+    if (ref.current) {
+      ref.current.focus();
+    }
+  });
 
-  const visableExpenses =
-    selectedCategory && selectedCategory !== "all"
-      ? expences.filter(
-          (expense) =>
-            expense.category.toLowerCase() === selectedCategory.toLowerCase()
-        )
-      : expences;
+  useEffect(() => {
+    document.title = "My App";
+  });
 
   return (
     <div>
-      <h1 className="text-center my-4">Expense Tracker</h1>
-      <div className="mb-5">
-        <ExpenseForm
-          onSubmit={(expense) => {
-            setExpences([...expences, { ...expense, id: expences.length + 1 }]);
-          }}
-        />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category) => {
-            setSelectedCategory(category);
-          }}
-        />
-      </div>
-      <ExpenseList
-        expenses={visableExpenses}
-        onDelete={(id) => {
-          setExpences(expences.filter((expense) => expense.id !== id));
-        }}
-      ></ExpenseList>
+      <input ref={ref} type="text" className="form-controle" />
     </div>
   );
 }
